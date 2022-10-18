b22af3b29102b1247381537135e8e8bcdaa766a8
Daniela de Almeida Lima <danielalimaa77@gmail.com>
2022-10-17 23:13:56 -0300

    first commit js

---
+++ maça.js
@@ -0,0 +1,17 @@
+   // exercicio pessoal comer maçã cortada ou não 
+
+
+   alert("Quiz maçãs!!")
+
+   let maçaCortada = prompt("Você quer sua maçã cortada? \n (Sim/Não)")
+
+  
+   if(maçaCortada === "Sim") {
+     alert("A maçã foi cortada, pronta para comer ")
+   } else if (maçaCortada === "Não") {
+    alert("A maçã não foi cortada, pronta para comer também ")
+   }
+
+   alert("Bon apetit!")
+  
+  
